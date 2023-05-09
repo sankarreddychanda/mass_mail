ad4357d8ea967c17fdbf30aec6ce20c6ebf41b87
jaya sankar <jayashankar@tech.netzary.com>
2023-05-09 17:12:43 +0530

    update

--- main/views.py
+++ main/views.py
@@ -10,28 +10,33 @@ import requests
 from massmail import settings
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
-
+from django.template.loader import render_to_string
 
 def send_mass_mail_confirm(request):
     if request.method == 'POST':
-        print(dir(request.POST))
         if 'csv_file' in request.FILES:
             csv_file = request.FILES['csv_file']
-            print(csv_file,'-------------')
-
             csv_data = csv.reader(csv_file.read().decode('utf-8').splitlines())
 
-            for row in csv_data:
+            # Get the selected template from the form data
+            template = request.POST.get('template')
 
+            for row in csv_data:
                 email = row[0]
                 name = row[0]
 
-                # Prepare email data
+                # Prepare email data using the selected template
+                context = {
+                    'name': name,
+                    'subject': 'Your Subject'
+                }
+                html_content = render_to_string(template, context)
+
                 data = {
                     'from': 'Your Name steve@affluencebizdata.com',
                     'to': email,
                     'subject': 'Your Subject',
-                    'html': f'Hello {name},<br><br>Your message goes here.'
+                    'html': html_content
                 }
 
                 # Send email using Mailgun API
@@ -41,7 +46,6 @@ def send_mass_mail_confirm(request):
                     data=data
                 )
 
-
                 if response.status_code == 200:
                     print(f'Successfully sent email to {email}')
                 else:
@@ -50,6 +54,49 @@ def send_mass_mail_confirm(request):
             return HttpResponseRedirect('/success/')
 
     return render(request, 'send_mass_mail.html')
+
+
+
+
+
+
+# def send_mass_mail_confirm(request):
+#     if request.method == 'POST':
+#         if 'csv_file' in request.FILES:
+#             csv_file = request.FILES['csv_file']
+#             csv_data = csv.reader(csv_file.read().decode('utf-8').splitlines())
+#             for row in csv_data:
+#                 email = row[0]
+#                 name = row[0]
+#                 # Prepare email data
+#                 context = {
+#                     'name': name,
+#                     'subject': 'Your Subject'
+#                 }
+#                 html_content = render_to_string('email_template_education.html', context)
+#
+#                 data = {
+#                     'from': 'Your Name steve@affluencebizdata.com',
+#                     'to': email,
+#                     'subject': 'Your Subject',
+#                     'html': html_content
+#                 }
+#
+#                 # Send email using Mailgun API
+#                 response = requests.post(
+#                     f'https://api.mailgun.net/v3/{settings.MAILGUN_DOMAIN}/messages',
+#                     auth=('api', settings.MAILGUN_API_KEY),
+#                     data=data
+#                 )
+#
+#                 if response.status_code == 200:
+#                     print(f'Successfully sent email to {email}')
+#                 else:
+#                     print(f'Error sending email to {email}: {response.content}')
+#
+#             return HttpResponseRedirect('/success/')
+#
+#     return render(request, 'send_mass_mail.html')
 
 def success(request):
     return HttpResponse('success')
